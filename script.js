c5d22bcca55a0ae88dd4417a05552cca86aa1fe8
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-01 11:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height)
+	{
+		this._height=height
+		this._width=width
+	}
+	getArea()
+	{
+		return (this._height)*(this._width)
+	}
+	get width(){
+		return this._width
+	}
+	get height(){
+		return this._height
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side)
+	{
+		super(side,side)
+		this._side=side
+	}
+	getPerimeter(){
+		return 4*(this._side)
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
